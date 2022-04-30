52a5bf30b9e32bdf647e3ee2be9c7040b1c5fb66
Daniel Davidraj <danieldavidraj23@gmail.com>
2022-04-30 10:19:21 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,35 @@
+from functools import wraps
+from flask import Flask, redirect, render_template, session
+import pymongo
+
+app = Flask(__name__)
+app.secret_key=b'\x95O\xa2\xca\x07)\x1a\x13HiEH\xff~\x9e\x1b'
+
+#Database
+client = pymongo.MongoClient('localhost', 27017)
+db = client.mydb
+
+
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
+  
+#Routes
+from user import routes
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/dashboard/")
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
